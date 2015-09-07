e45bffc56fe1629275c1166406f1b2d9af688381
Guido Schmidt <guidoschmidt@uni-koblenz.de>
2015-09-07 17:06:12 +0200

    added promieses example

---
+++ promises/promises.js
@@ -0,0 +1,16 @@
+console.log('Javascript Promises');
+
+var container = $('#container');
+
+var promise = $.get('http://localhost:8080/test.json');
+
+promise
+.success(function(data) {
+  data.map(function(object, index) {
+    console.log(object.name);
+    container.append('<h3>' + object.name + ' (' + object.age + ')</h3>');
+  });
+})
+.error(function(xhr, status, error) {
+  console.log(xhr + ' | ' + status + ' : ' + error);
+});
